9bde9580cf8b14f8a65222a12fd34314c449fa44
Felix S <felix@gnampfelix.de>
2018-01-15 22:26:41 +0100

    Add many Vector3 functionalities

--- geometry/Vector3_test.go
+++ geometry/Vector3_test.go
@@ -14,4 +14,64 @@ var _ = Describe("Vector3", func() {
 		Expect(a.Y()).Should(Equal(20.0))
 		Expect(a.Z()).Should(Equal(30.0))
 	})
+
+	It("should add a Vector3", func() {
+		a := NewVector3(10, 20, 30)
+		a.Add(a)
+		Expect(a.X()).Should(Equal(20.0))
+		Expect(a.Y()).Should(Equal(40.0))
+		Expect(a.Z()).Should(Equal(60.0))
+	})
+
+	It("should subtract a Vector3", func() {
+		a := NewVector3(10, 20, 30)
+		a.Subtract(a)
+		Expect(a.X()).Should(Equal(0.0))
+		Expect(a.Y()).Should(Equal(0.0))
+		Expect(a.Z()).Should(Equal(0.0))
+	})
+
+	It("should calculate the length of the Vector3", func() {
+		a := NewVector3(3, 0, 4)
+		l := a.Length()
+		Expect(l).Should(Equal(5.0))
+	})
+
+	It("should calculate the cross product of two vectors", func() {
+		a := NewVector3(1, 2, 3)
+		b := NewVector3(3, 2, 1)
+		c := CrossProduct(a, b)
+		Expect(c.X()).Should(Equal(-4.0))
+		Expect(c.Y()).Should(Equal(8.0))
+		Expect(c.Z()).Should(Equal(-4.0))
+	})
+
+	It("should multiply the Vector3 with a scalar", func() {
+		a := NewVector3(10, 20, 30)
+		a.MultiplyWithScalar(3)
+		Expect(a.X()).Should(Equal(30.0))
+		Expect(a.Y()).Should(Equal(60.0))
+		Expect(a.Z()).Should(Equal(90.0))
+	})
+
+	It("should calculate the scalar product", func() {
+		a := NewVector3(10, 20, 30)
+		scalarProduct := ScalarProduct3(a, a)
+		Expect(scalarProduct).Should(Equal(1400.0))
+	})
+
+	It("should calculate the angle between two Vector3", func() {
+		a := NewVector3(3, 2, 1)
+		b := NewVector3(1, 2, 3)
+		angle := AngleBetween(a, b)
+		Expect(angle).Should(BeNumerically("~", 0.77519337331036130720409371118247))
+	})
+
+	It("should normalize a vector", func() {
+		a := NewVector3(3, 0, 4)
+		a.Normalize()
+		Expect(a.X()).Should(BeNumerically("~", 3.0/5.0))
+		Expect(a.Y()).Should(BeNumerically("~", 0.0/5.0))
+		Expect(a.Z()).Should(BeNumerically("~", 4.0/5.0))
+	})
 })
--- geometry/vector3.go
+++ geometry/vector3.go
@@ -1,5 +1,9 @@
 package geometry
 
+import (
+	"math"
+)
+
 // A Vector3 represents a vector in the three dimensional space.
 type Vector3 struct {
 	x, y, z float64
@@ -27,4 +31,60 @@ func (v Vector3) Y() float64 {
 // Z returns the z component of the vector3.
 func (v Vector3) Z() float64 {
 	return v.z
+}
+
+// Add adds the Vector3 b to the vector by adding all of its components.
+func (v *Vector3) Add(b Vector3) {
+	v.x += b.x
+	v.y += b.y
+	v.z += b.z
+}
+
+// Subtract subtracts the vector b from the vector by subtracting all of its
+// components
+func (v *Vector3) Subtract(b Vector3) {
+	v.x -= b.x
+	v.y -= b.y
+	v.z -= b.z
+}
+
+// MultiplyWithScalar multiplies the vector with the given scalar.
+func (v *Vector3) MultiplyWithScalar(s float64) {
+	v.x *= s
+	v.y *= s
+	v.z *= s
+}
+
+// Length calculates the length of the vector.
+func (v Vector3) Length() float64 {
+	sumOfSquares := math.Pow(v.X(), 2) + math.Pow(v.Y(), 2) + math.Pow(v.Z(), 2)
+	return math.Sqrt(sumOfSquares)
+}
+
+func (v *Vector3) Normalize() {
+	norm := v.Length()
+	v.MultiplyWithScalar(1.0 / norm)
+}
+
+// ScalarProduct3 calculates the scalar (dot) product of two vector3.
+func ScalarProduct3(a, b Vector3) float64 {
+	result := a.X() * b.X()
+	result += a.Y() * b.Y()
+	result += a.Z() * b.Z()
+	return result
+}
+
+// CrossProduct returns the cross product of two vectors.
+func CrossProduct(a, b Vector3) Vector3 {
+	newX := a.Y()*b.Z() - a.Z()*b.Y()
+	newY := a.Z()*b.X() - a.X()*b.Z()
+	newZ := a.X()*b.Y() - a.Y()*b.X()
+
+	return NewVector3(newX, newY, newZ)
+}
+
+func AngleBetween(a, b Vector3) float64 {
+	numerator := ScalarProduct3(a, b)
+	denominator := a.Length() * b.Length()
+	return math.Acos(numerator / denominator)
 }
